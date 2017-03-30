1f9b606441f6e7eadabe7b53a7a1d02c9715adc7
alexpres <alexpresman@gmail.com>
2017-03-30 09:48:28 -0400

    Assignment 2

--- assignment_1/assignment1.js
+++ assignment_1/assignment1.js
@@ -3,6 +3,10 @@ Exercise #1
 Initialize an empty array below in the variable numbers.
 */
 var numbers;
+
+
+var numbers = [];
+  
 console.log('hello world');
 
 /*
@@ -10,9 +14,11 @@ Exercise #2
 Using the loop provided for you below, use an array's push method to insert the nummbers  0 to 10 into positions 0 through 10 of the numbers array you just initialized above.'If you're not sure how to use the array push method go back and re read this module on data strucutres.'
 */
 
-for(var i=0;i<10;i++){
+
+for(var i=0;i<10;i++){numbers.push(i)}
+console.log(numbers);
     //use the array push() method here.
-}
+
 
 
 /*
--- assignment_2/assignment2.js
+++ assignment_2/assignment2.js
@@ -5,6 +5,10 @@ Create a for loop that prints out the numbers 1 to 100 in the console.
 
 
 
+for(var i=0;i<101;i++)
+console.log(i) ;
+
+
 /*
 Exercise #2
 Write a loop that makes seven calls to console.log to output the following triangle:
@@ -17,3 +21,11 @@ Write a loop that makes seven calls to console.log to output the following trian
 ######
 #######
 */
+
+
+var content = '';
+
+for (var i = 0; i < 7; i++) {
+    content = content + '#';
+    console.log(content);
+}
